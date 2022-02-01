3cfb075a998be815b4c3e73a1ee211fd162869ae
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-01 17:05:30 +0000

    Add: next-with-less
    This package enables us to use AntDesign (which uses less under the hood)
    with NextJS (which uses less under the hood)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-}
+const withLess = require('next-with-less')
+
+module.exports = withLess({
+	// Less config
+	lessLoaderOptions: {
+		lessOptions: {
+			modifyVars: {},
+			javascriptEnabled: true,
+		},
+	},
+	// NextJS config
+	reactStrictMode: true,
+	basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+})
