a46e7b23e2aef7fc8cc094578fbbb0ec88ddfc92
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-02 14:53:54 +0000

    add: correct monospaced and sans serif fonts
    Fonts and weights are based on the design documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,27 @@ module.exports = {
 		extend: {
 			colors: theme.colors,
 			fontFamily: {
+				sans: [
+					"Poppins",
+					...[
+						"ui-sans-serif",
+						"system-ui",
+						"-apple-system",
+						"BlinkMacSystemFont",
+						"Segoe UI",
+						"Roboto",
+						"Helvetica Neue",
+						"Arial",
+						"Noto Sans",
+						"sans-serif",
+						"Apple Color Emoji",
+						"Segoe UI Emoji",
+						"Segoe UI Symbol",
+						"Noto Color Emoji",
+					],
+				],
 				mono: [
-					"'Jetbrains Mono'",
+					"'IBM Plex Mono'",
 					...[
 						"ui-monospace",
 						"SFMono-Regular",
