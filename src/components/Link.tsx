aa466c0c536de53518b532553821e06b75fa413a
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-08 17:54:41 +0000

    add: className prop

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -7,12 +7,13 @@ interface LinkProps {
 	icon?: IconNames;
 	to: string;
 	as?: string;
+	className?: string;
 }
 
-export const Link = ({ label, icon, as, to, ...props }: LinkProps) => {
+export const Link = ({ label, icon, as, to, className = "", ...props }: LinkProps) => {
 	return (
 		<NextLink href={to} as={as}>
-			<a className="no-underline font-bold text-inherit text-base" {...props}>
+			<a className={`no-underline font-bold text-inherit text-base ${className}`} {...props}>
 				<div className="flex items-center space-x-3">
 					<Icon name={icon} className="w-[18px] h-[18px]" />
 					<div>{label}</div>
