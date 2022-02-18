b1fcc04c53066958585c967830151af1695229fe
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-18 14:11:37 +0000

    test: update snapshots

--- __tests__/InvestmentDetails.test.tsx
+++ __tests__/InvestmentDetails.test.tsx
@@ -2,12 +2,28 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { InvestmentDetails } from "@components/InvestmentDetails";
 
-test("Default", () => {
+test("Positive return", () => {
 	const props = {
 		balance: 65,
-		balance_currency: "USDC",
+		balanceCurrency: "USDC",
 		investments: 256,
-		investments_currency: "USDC",
+		investmentsCurrency: "USDC",
+		investmentsReturn: 3.43,
+	};
+
+	const component = renderer.create(<InvestmentDetails {...props} />);
+
+	const tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test("Negative return", () => {
+	const props = {
+		balance: 65,
+		balanceCurrency: "USDC",
+		investments: 256,
+		investmentsCurrency: "USDC",
+		investmentsReturn: -3.43,
 	};
 
 	const component = renderer.create(<InvestmentDetails {...props} />);
