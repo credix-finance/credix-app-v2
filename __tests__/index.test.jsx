3850b33a9efabb9718518b66fc154b54ba64d9d3
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-18 16:29:58 +0000

    test: update snapshots

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,8 +1,11 @@
-import { render } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import Overview from "../src/pages/index";
 
 describe("Home", () => {
 	it("renders a heading", () => {
-		render(<Overview />);
+		const component = renderer.create(<Overview />);
+
+		const tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
 	});
 });
