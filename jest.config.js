4c29d88cea47d21edef2eb60b14f4d7766b4bdfb
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-22 15:04:55 +0000

    refactor: move matchMedia mock to jest setup script

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,6 @@ const customJestConfig = {
 	moduleNameMapper: {
 		"^@components/(.*)": "<rootDir>/src/components/$1",
 	},
-	modulePathIgnorePatterns: ["__mocks__"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
