129d6609f077d91a852c7efcceee1145a7dcb417
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-23 12:18:29 +0000

    feat: add client integration
    A client provider is added as we can't use the `useConnection` hook from the same component where the connectionprovider is used.

---
+++ src/components/ClientProvider.tsx
@@ -0,0 +1,20 @@
+import React, { FC, ReactNode } from "react";
+import { CredixClientProvider } from "@credix/credix-client";
+import { Wallet } from "@project-serum/anchor";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { config } from "config";
+
+export const ClientProvider: FC<{ children: ReactNode }> = ({ children }) => {
+	const { connection } = useConnection();
+	const wallet = useWallet();
+
+	return (
+		<CredixClientProvider
+			connection={connection}
+			wallet={wallet as typeof Wallet}
+			config={{ programId: config.clusterConfig.programId, confirmOptions: config.confirmOptions }}
+		>
+			{children}
+		</CredixClientProvider>
+	);
+};
