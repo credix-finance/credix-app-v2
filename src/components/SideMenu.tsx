b6b1c43b76ac5e99d0fbaa6d1f446b08046edaa1
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-25 10:08:02 +0000

    feat: automatically highlight active sidemenu item

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,18 +1,23 @@
+import { useRouter } from "next/router"
 import React from "react";
+import { CredixLogo } from "./CredixLogo"
 import { SideMenuItem, SideMenuItemProps } from "./SideMenuItem";
 
 interface SideMenuProps {
-	menuItems: SideMenuItemProps[];
+	menuItems: Omit<SideMenuItemProps, "isActive">[];
 }
 
 export const SideMenu = ({ menuItems }: SideMenuProps) => {
+	const router = useRouter();
+
 	return (
-		<div className="w-72 bg-darker h-screen pt-[33px]">
-			{/* TODO: replace this with CredixIcon component when merged */}
-			<div className="w-[39px] h-[39px] text-credix-primary ml-[43px]">CRDX</div>
+		<div className="w-72 bg-darker h-screen pt-[33px] sticky top-0">
+			<div className="w-[39px] h-[39px] text-credix-primary ml-[43px]">
+				<CredixLogo mode="light" />
+			</div>
 			<div className="mt-[145px]">
 				{menuItems.map((item) => (
-					<SideMenuItem key={item.label} {...item} />
+					<SideMenuItem key={item.label} isActive={router.pathname === item.path} {...item} />
 				))}
 			</div>
 		</div>
