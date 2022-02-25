13e3c76801974f4f492db33bb10f168b027aae71
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-25 10:04:29 +0000

    feat: optionally display credix logo
    This is useful for when the main menu is displayed alongside the sidemenu. As the sidemenu already contains the logo.

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -5,11 +5,15 @@ import { CredixLogo } from "@components/CredixLogo";
 import { WalletButton } from "@components/WalletButton";
 import { IdentityButton } from "@components/IdentityButton";
 
-export const MainMenu = () => {
+interface MainMenuProps {
+	showLogo?: boolean;
+}
+
+export const MainMenu = ({showLogo = true}: MainMenuProps) => {
 	return (
 		<div className="w-full bg-neutral-0 flex justify-between items-center py-[16.5px] px-4 lg:px-[88px]">
 			<div>
-				<CredixLogo />
+				{showLogo && <CredixLogo />}
 			</div>
 			<div className="md:flex">
 				{config.clusterConfig.name !== SolanaCluster.LOCALNET && <IdentityButton />}
