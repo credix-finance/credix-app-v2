4a0d27912323187d939f552ac7c994a25578ee5f
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-02-25 10:01:55 +0000

    feat: add hover state

--- src/components/SideMenuItem.tsx
+++ src/components/SideMenuItem.tsx
@@ -12,11 +12,13 @@ export interface SideMenuItemProps {
 export const SideMenuItem = ({ label, icon, path, isActive }: SideMenuItemProps) => {
 	return (
 		<Link href={path} key={label}>
-			<a className={`h-[60px] flex items-center ${isActive ? "bg-credix-primary" : "bg-darker"}`}>
+			<a className={`h-[60px] flex items-center ${isActive ? "bg-credix-primary" : "bg-darker"} group hover:bg-neutral-10`}>
 				<div
-					className={`border-0 h-8 border-l-[3px] border-darker border-solid w-full pl-[23px] flex items-center ${
-						isActive ? "text-darker font-bold" : "text-credix-primary font-medium"
-					}`}
+					className={`
+						border-0 h-8 border-l-[3px] border-darker border-solid w-full pl-[23px] flex items-center
+						group-hover:border-neutral-10 group-hover:text-darker group-hover:text-bold
+						${ isActive ? "text-darker font-bold" : "text-credix-primary font-medium"}
+					`}
 				>
 					<Icon name={icon} className="w-[20px] h-[20px]" />
 					<span className="pl-[18px] text-base">{label}</span>
