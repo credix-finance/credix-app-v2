c585329cf09363e2530f826aa0ea7373037a18ec
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-03-18 10:41:07 +0000

    feat: update main menu and menu buttons bg color

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -7,7 +7,7 @@ import { IdentityButton } from "@components/IdentityButton";
 
 export const MainMenu = () => {
 	return (
-		<div className="w-full bg-neutral-0 flex justify-between items-center py-[16.5px] px-4 lg:px-[88px]">
+		<div className="w-full bg-credix-primary flex justify-between items-center py-[16.5px] px-4 lg:px-[88px]">
 			<div>
 				<CredixLogo />
 			</div>
