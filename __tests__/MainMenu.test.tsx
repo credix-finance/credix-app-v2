c1774a62166a6b1edb14747ef698546b4880b004
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-03-21 17:04:37 +0000

    test: add wallet context

--- __tests__/MainMenu.test.tsx
+++ __tests__/MainMenu.test.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { MainMenu } from "@components/MainMenu";
+import { WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 test("Main menu", () => {
-	const component = renderer.create(<MainMenu />);
+	const component = renderer.create(
+		<WalletProvider wallets={[]} autoConnect>
+			<WalletModalProvider>
+				<MainMenu />
+			</WalletModalProvider>
+		</WalletProvider>
+	);
 
 	const tree = component.toJSON();
 	expect(tree).toMatchSnapshot();
