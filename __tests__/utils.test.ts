382bbb4b775bfac251db2310a885e8c771be4c0e
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-03-21 11:50:17 +0000

    test: add clamp, numberformat and ratioformat tests

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,5 +1,12 @@
-import { formatDate, formatTimestamp, round } from "@utils/format.utils";
 import Big from "big.js";
+import {
+	formatDate,
+	formatTimestamp,
+	round,
+	clamp,
+	numberFormatter,
+	ratioFormatter,
+} from "@utils/format.utils";
 
 describe("date formatting", () => {
 	it("formats a date", () => {
@@ -21,7 +28,7 @@ describe("date formatting", () => {
 	});
 });
 
-describe("formatting", () => {
+describe("Format utils", () => {
 	test("Big numbers get rounded correctly", async () => {
 		const value = new Big(100.1234);
 		const expected = new Big(100.123);
@@ -34,5 +41,68 @@ describe("formatting", () => {
 		const expected = new Big(100.12);
 
 		expect(round(value, Big.roundHalfEven).eq(expected)).toBeTruthy();
+	});
+
+	test("clamp value less than min returns min", () => {
+		const value = 10;
+		const min = 20;
+		const max = 100;
+		const clampedValue = clamp(value, min, max);
+
+		expect(clampedValue).toBe(min);
+	});
+
+	test("clamp value greater than max returns max", () => {
+		const value = 200;
+		const min = 20;
+		const max = 100;
+		const clampedValue = clamp(value, min, max);
+
+		expect(clampedValue).toBe(max);
+	});
+
+	test("clamp value between min and max returns value", () => {
+		const value = 50;
+		const min = 20;
+		const max = 100;
+		const clampedValue = clamp(value, min, max);
+
+		expect(clampedValue).toBe(value);
+	});
+
+	test("numbers representing currency are formatted correctly", () => {
+		const expected = "200.3";
+		let value = 200.3;
+		let formattedValue = numberFormatter.format(value);
+
+		expect(formattedValue).toBe(expected);
+
+		value = 200.25;
+		formattedValue = numberFormatter.format(value);
+
+		expect(formattedValue).toBe(expected);
+
+		value = 200.34;
+		formattedValue = numberFormatter.format(value);
+
+		expect(formattedValue).toBe(expected);
+	});
+
+	test("numbers representing percentages are formatted correctly", () => {
+		const expected = "12.3%";
+		let value = 0.123;
+		let formattedValue = ratioFormatter.format(value);
+
+		expect(formattedValue).toBe(expected);
+
+		value = 0.1234;
+		formattedValue = ratioFormatter.format(value);
+
+		expect(formattedValue).toBe(expected);
+
+		value = 0.1229;
+		formattedValue = ratioFormatter.format(value);
+
+		expect(formattedValue).toBe(expected);
 	});
 });
