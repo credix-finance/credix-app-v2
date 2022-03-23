2d76392a5101b8355a39198498fe6b486c2554a3
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-03-23 14:03:26 +0000

    feat: don't show investmentreturn component

--- src/components/InvestmentDetails.tsx
+++ src/components/InvestmentDetails.tsx
@@ -20,7 +20,7 @@ export const InvestmentDetails = ({
 	investmentsReturn,
 }: InvestmentDetailsProps) => {
 	return (
-		<div className="md:flex space-y-2 md:space-y-0 md:space-x-4">
+		<div className="md:grid md:grid-cols-2 space-y-2 md:space-y-0 md:space-x-4">
 			<div className="rounded border border-solid border-neutral-40 bg-neutral-0 p-6 grid place-items-center md:block">
 				<div className="text-xs md:text-base font-medium">Current balance</div>
 				<div className="text-2xl font-bold">{`${
@@ -30,7 +30,8 @@ export const InvestmentDetails = ({
 			<div className="rounded border border-solid border-neutral-40 bg-neutral-0 p-6 grid place-items-center md:block">
 				<div className="flex justify-between items-start md:space-x-24 md:justify-start">
 					<div className="text-xs md:text-base font-medium">Current investments</div>
-					<InvestmentReturn value={investmentsReturn} className="hidden md:flex" />
+					{/* TODO: show this component when the endpoint is implemented */}
+					{false && <InvestmentReturn value={investmentsReturn} className="hidden md:flex" />}
 				</div>
 				<div>
 					<div className="text-2xl font-bold">{`${
