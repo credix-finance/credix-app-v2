0ba943bc3e276bc9e7f3639a5b536deb1167fd5c
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-03-29 14:50:02 +0100

    feat: redirect user after create

--- src/pages/[marketplace]/deals/new.tsx
+++ src/pages/[marketplace]/deals/new.tsx
@@ -31,43 +31,45 @@ const New: NextPageWithLayout = () => {
 	}: DealFormInput) => {
 		const formattedPrincipal = numberFormatter.format(principal);
 		const hide = message.loading({ content: `Creating deal for ${formattedPrincipal} USDC` });
+		const borrowerPK = new PublicKey(borrower);
+		const credixPass = await market.fetchCredixPass(borrowerPK);
+
+		if (!credixPass) {
+			hide();
+			message.error({ content: "No Credix Pass found for given public key" });
+			return;
+		}
 
 		try {
-			const borrowerPK = new PublicKey(borrower);
-			const credixPass = await market.fetchCredixPass(borrowerPK);
-
-			if (!credixPass) {
-				hide();
-				message.error({ content: "No Credix Pass found for given public key" });
-				return;
-			}
-
-			if (credixPass.isActive) {
-				hide();
-				message.error({ content: `Credix Pass is not active` });
-				return;
-			}
-
 			const principalProgramAmount = toProgramAmount(new Big(principal)).toNumber();
-			const dealAddress = await market.createDeal(
+			await market.createDeal(
 				principalProgramAmount,
 				financingFee,
 				timeToMaturity,
 				borrowerPK,
 				dealName
 			);
-
 			hide();
 			message.success({ content: `Successfully created deal for ${formattedPrincipal} USDC` });
-
-			router.push(`/${marketplace}/deals/${dealAddress}/show`);
-		} catch (e) {
+		} catch {
+			hide();
 			message.error({ content: `Failed to create deal for ${formattedPrincipal} USDC` });
+			return;
+		}
+
+		try {
+			const borrowerInfo = await market.fetchBorrowerInfo(borrowerPK);
+			const deal = await borrowerInfo.fetchDeal(borrowerInfo.numberOfDeals - 1);
+
+			router.push(`/${marketplace}/deals/${deal.address.toString()}/show`);
+		} catch {
+			message.error({ content: `Failed to get deal info` });
+			router.push(`/${marketplace}/deals`);
 		}
 	};
 
 	return (
-		<div className="py-5 px-4 md:pt-12 md:px-28">
+		<div className="py-5 px-4 md:pt-12 md:px-20 md:grid md:justify-self-center md:w-full md:max-w-7xl lg:max-w-5xl">
 			<Link to={`/${marketplace}/deals`} label="Go back to all deals" icon="chevron-left-square" />
 			<div className="text-4xl font-sans pt-3 pb-5">New Deal</div>
 			<div className="bg-neutral-0 py-10 px-24 space-y-7">
