85dba0e227cc4cd67098e4d82f74d728eacc2884
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-03-29 09:52:29 +0100

    test: simplify resetting of store after each test run

--- __tests__/__mocks__/zustand.ts
+++ __tests__/__mocks__/zustand.ts
@@ -6,24 +6,19 @@ import actualCreate from "zustand";
 import { act } from "react-dom/test-utils";
 import { StoreState } from "@state/useStore";
 
-// a variable to hold reset functions for all stores declared in the app
-const storeResetFns = new Set();
+let reset: () => void;
 
-// when creating a store, we get its initial state, create a reset function and add it in the set
+// when creating a store, we get its initial state and create a reset function
 const create = (createState) => {
 	const store = actualCreate<StoreState>(createState);
 	const initialState = store.getState();
-	storeResetFns.add(() => store.setState(initialState, true));
+	reset = () => store.setState(initialState, true);
 	return store;
 };
 
-// Reset all stores after each test run
+// Reset store after each test run
 afterEach(() => {
-	act(() =>
-		storeResetFns.forEach((resetFn) => {
-			resetFn();
-		})
-	);
+	act(() => reset());
 });
 
 export default create;
