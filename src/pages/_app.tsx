a01b6901233b5793b8d49747940e1c5833e23656
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-04-06 11:18:25 +0100

    fix: run eslint formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,10 +11,10 @@ import {
 	TorusWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import { config } from "config";
-import { NextPage } from "next"
-import { ClientProvider } from "@components/ClientProvider"
-import { useRouter } from "next/router"
-import { pageview } from "@utils/analytics.utils"
+import { useRouter } from "next/router";
+import { pageview } from "@utils/analytics.utils";
+import { NextPage } from "next";
+import { ClientProvider } from "@components/ClientProvider";
 
 // Use require instead of import since order matters
 require("@solana/wallet-adapter-react-ui/styles.css");
@@ -22,30 +22,30 @@ require("../styles/antd.less");
 require("../styles/globals.css");
 
 export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+	getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+	Component: NextPageWithLayout;
+};
 
 const CredixApp: FC<AppProps> = ({ Component, pageProps }: AppPropsWithLayout) => {
-	const router = useRouter()
+	const router = useRouter();
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      pageview(url)
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			pageview(url);
+		};
+		//When the component is mounted, subscribe to router changes
+		//and log those page views
+		router.events.on("routeChangeComplete", handleRouteChange);
 
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 
 	// @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
 	// Only the wallets you configure here will be compiled into your application, and only the dependencies
@@ -61,8 +61,8 @@ const CredixApp: FC<AppProps> = ({ Component, pageProps }: AppPropsWithLayout) =
 		[]
 	);
 
-	const getLayout = Component.getLayout ?? ((page) => page)
-	const layoutComponent = getLayout(<Component {...pageProps} />)
+	const getLayout = Component.getLayout ?? ((page) => page);
+	const layoutComponent = getLayout(<Component {...pageProps} />);
 
 	return (
 		<>
@@ -74,9 +74,7 @@ const CredixApp: FC<AppProps> = ({ Component, pageProps }: AppPropsWithLayout) =
 			<ConnectionProvider endpoint={config.clusterConfig.RPCEndpoint}>
 				<WalletProvider wallets={wallets} autoConnect>
 					<WalletModalProvider>
-						<ClientProvider>
-							{layoutComponent}
-						</ClientProvider>
+						<ClientProvider>{layoutComponent}</ClientProvider>
 					</WalletModalProvider>
 				</WalletProvider>
 			</ConnectionProvider>
