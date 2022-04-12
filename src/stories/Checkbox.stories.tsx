98f55e5bddd1c8d6fb0b7b40e1f3c96e268f16b1
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-04-12 14:59:07 +0100

    feat: add checkbox component

---
+++ src/stories/Checkbox.stories.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Checkbox } from "@components/Checkbox";
+import Form from "antd/lib/form/Form";
+import { Button } from "@components/Button";
+
+export default {
+	title: "Checkbox",
+	component: Checkbox,
+} as ComponentMeta<typeof Checkbox>;
+
+const Template: ComponentStory<typeof Checkbox> = (args) => <Checkbox name="test" {...args} />;
+
+const defaultArgs = {
+	label: "Simple answer to select",
+};
+
+export const Default = Template.bind({});
+Default.decorators = [
+	(Story) => (
+		<Form layout="vertical" className="w-[400px]">
+			<Story />
+		</Form>
+	),
+];
+Default.args = {
+	...defaultArgs,
+};
+
+export const Checked = Template.bind({});
+Checked.decorators = [
+	(Story) => (
+		<Form layout="vertical" initialValues={{ test: true }} className="w-[400px]">
+			<Story />
+		</Form>
+	),
+];
+Checked.args = {
+	...defaultArgs,
+};
