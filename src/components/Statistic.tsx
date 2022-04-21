4228005e5289fa43091204d1b6257b413533b4ce
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-04-21 14:23:39 +0200

    feat: make marketstats and cards transparent

--- src/components/Statistic.tsx
+++ src/components/Statistic.tsx
@@ -24,10 +24,11 @@ export const Statistic = ({ label, value, currency, isPercentage = false }: Stat
 	}, [formatter, value]);
 
 	return (
-		<div className="bg-credix-primary border border-solid border-darker rounded-[1px] font-sans h-36 w-min min-w-[16rem] md:w-full md:min-w-[12rem] ml-[21.5px] pr-5 flex items-center">
-			<div className="bg-credix-primary ml-[-21.5px]">
-				<div className="capitalize">{label}</div>
-				<div className="text-6xl font-bold">
+		<div className="bg-transparent border border-solid border-darker rounded-[1px] font-sans h-36 w-min min-w-[16rem] md:w-full md:min-w-[12rem] ml-[21.5px] pr-5 flex items-center">
+			<div className="bg-transparent ml-[-21.5px] relative">
+				<div className="left-5 absolute w-3 h-20 z-10 bg-credix-primary"></div>
+				<div className="capitalize relative z-20">{label}</div>
+				<div className="text-6xl font-bold relative z-20">
 					{formattedValue}
 					<span className="text-sm font-normal">{currency}</span>
 				</div>
