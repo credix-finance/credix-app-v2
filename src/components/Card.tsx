1d2724ff83ea9914ec0723ebe23be944979f838f
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-04-22 16:14:35 +0200

    fix: card story baseline

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,7 +17,7 @@ export const Card = ({ topTitle, title, offset = "middle", children }: CardProps
 	return (
 		<div className="md:col-span-3 border border-solid rounded-[1px] border-darker w-full p-7 pt-16 bg-transparent">
 			<div className={`bg-transparent ${offsetValues[offset]} py-[18.5px] w-max relative`}>
-				<div className="left-5 absolute w-3 h-20 z-10 bg-credix-primary"></div>
+				<div className="left-4 absolute top-1 w-3 h-20 z-10 bg-credix-primary"></div>
 				<div className="text-xs font-normal capitalize relative z-20">{topTitle}</div>
 				<div className="font-sans font-semibold text-[32px] capitalize relative z-20">{title}</div>
 			</div>
