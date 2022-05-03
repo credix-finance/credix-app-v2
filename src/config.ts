fb352c6007ca94a75411cf213db43dd74e093e3d
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-05-03 16:21:58 +0200

    test: fix tests failing because of missing env var

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,9 @@ import { ClusterConfig, Config } from "@credix_types/config.types";
 import { RPCEndpoint, SolanaCluster } from "@credix_types/solana.types";
 
 const baseConfig = {
-	programId: new PublicKey(process.env.NEXT_PUBLIC_PROGRAM_ID),
+	programId: new PublicKey(
+		process.env.NEXT_PUBLIC_PROGRAM_ID || "CRDx2YkdtYtGZXGHZ59wNv1EwKHQndnRc1gT4p8i2vPX"
+	),
 	gatewayProgramId: new PublicKey("gatem74V238djXdzWnJf94Wo1DcnuGkfijbf3AuBhfs"),
 };
 
