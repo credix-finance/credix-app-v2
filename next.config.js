90d7688d6d1ab2bb88277aa9920be75f3668ed5f
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-05-13 11:10:30 +0200

    feat: enable react strict mode as antd now supports it

--- next.config.js
+++ next.config.js
@@ -7,14 +7,7 @@ module.exports = withLess({
 		lessOptions: theme.lessOptions,
 	},
 	// NextJS config
-	/**
-	 * At the time of this writing AntdDesign does not support React 18 and StrictMode.
-	 * This causes the form validation to break and crash the application.
-	 * See: https://github.com/ant-design/ant-design/issues/26136 for more information.
-	 * We disable strict mode for now and will monitor the issue.
-	 * Bart De Caluwe - 29/03/2022
-	 */
-	reactStrictMode: false,
+	reactStrictMode: true,
 	basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 	trailingSlash: true,
