2220177e890007c3b2d763bd431da933036b07d2
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-05-16 17:27:05 +0200

    feat: add repayment graph and table utils

---
+++ src/utils/repayment.utils.ts
@@ -0,0 +1,34 @@
+import { RepaymentScheduleAmountType } from "@components/RepaymentScheduleGraph";
+import { Repayment } from "@utils/amortization.utils";
+
+export const generateGraphAndTableData = (schedule: Repayment[]) => {
+	const currentDate = new Date();
+
+	return schedule.reduce(
+		(acc, repayment, index) => {
+			const { interest, principal, balance } = repayment;
+			const month = (index + 1).toString();
+
+			acc.graphData.push({
+				month,
+				type: RepaymentScheduleAmountType.INTEREST,
+				amount: interest,
+			});
+			acc.graphData.push({
+				month,
+				type: RepaymentScheduleAmountType.PRINCIPAL,
+				amount: principal,
+			});
+
+			acc.dataSource.push({
+				date: new Date(currentDate.getFullYear(), index + 1, 1),
+				principal: principal.toString(),
+				interest: interest.toString(),
+				balance: balance.toString(),
+			});
+
+			return acc;
+		},
+		{ graphData: [], dataSource: [] }
+	);
+};
