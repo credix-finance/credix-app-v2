8478a796b26c5546c42d84fefc39a6d3fedba9e6
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-05-31 10:52:27 +0200

    docs: add comment regarding mocking of phantom

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -13,11 +13,16 @@ declare global {
 }
 
 Cypress.on("window:before:load", (win) => {
-	console.log(Cypress.env("MGMT_KEY"));
 	const key = new Uint8Array(Cypress.env("MGMT_KEY"));
 	const payer = Keypair.fromSecretKey(key);
 	const wallet = PhantomWalletMock.create(RPCEndpoint.DEVNET, payer, "confirmed");
 
+	/**
+	 * "Phantom's browser extension and mobile in-app browser will both inject a solana object into the
+	 * window of any web application the user visits."
+	 * From the [docs (2022/05/31)](https://docs.phantom.app/integrating/extension-and-mobile-browser/detecting-the-provider)
+	 * We replace it with a mock wallet so we can sign transactions without user interaction.
+	 */
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	win.solana = wallet;
