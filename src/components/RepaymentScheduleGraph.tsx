c91e9e3c334508b2839e135501ee648e8b1370dc
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-06-01 17:53:03 +0200

    feat: add custom tooltip

--- src/components/RepaymentScheduleGraph.tsx
+++ src/components/RepaymentScheduleGraph.tsx
@@ -21,6 +21,32 @@ export const RepaymentScheduleGraph: FunctionComponent<RepaymentScheduleGraphPro
 		colorField: "type",
 		color: [Theme.colors.credix.secondary, Theme.colors.neutral["20"]],
 		label: false as unknown as object,
+		tooltip: {
+			customContent: (title, data) => {
+				const [interest, principal] = data;
+
+				return `<div style="padding: 8px; width: 120px;">
+										<div style="display: flex; justify-content: space-between">
+											<div>Period: </div>
+											<div>${title}</div>
+										</div>
+										<div style="display: flex; justify-content: space-between; margin-top: 16px; margin-bottom: 8px">
+											<div style="display: flex; align-items: center;">
+												<div style="width: 8px; height: 8px; border-radius: 999999px; background-color: ${interest?.mappingData.color}"></div>
+												<div style="margin-left: 8px">Interest: </div>
+											</div>
+											<div>${interest?.value}</div>
+										</div>
+										<div style="display: flex; justify-content: space-between">
+											<div style="display: flex; align-items: center;">
+												<div style="width: 8px; height: 8px; border-radius: 999999px; background-color: ${principal?.mappingData.color}"></div>
+												<div style="margin-left: 8px">Principal: </div>
+											</div>
+											<div>${principal?.value}</div>
+										</div>
+								<div>`;
+			},
+		},
 	};
 
 	return <Column {...config} />;
