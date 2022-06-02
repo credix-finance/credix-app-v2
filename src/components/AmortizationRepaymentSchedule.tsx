ce93bb5d791b3fbace798b24db1e9c9ab530673e
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-06-02 10:45:45 +0200

    refactor: remove test data

--- src/components/AmortizationRepaymentSchedule.tsx
+++ src/components/AmortizationRepaymentSchedule.tsx
@@ -1,13 +1,10 @@
 import React, { FunctionComponent, ReactNode, useState } from "react";
 import { Input } from "@components/Input";
-import { RepaymentScheduleGraph } from "./RepaymentScheduleGraph";
-import { RepaymentScheduleTable } from "./RepaymentScheduleTable";
-import { Button } from "./Button";
-import { ColumnsProps } from "./Table";
-import {
-	RepaymentScheduleAmountType,
-	RepaymentScheduleDataPoint,
-} from "@credix_types/repaymentschedule.types";
+import { RepaymentScheduleGraph } from "@components/RepaymentScheduleGraph";
+import { RepaymentScheduleTable } from "@components/RepaymentScheduleTable";
+import { Button } from "@components/Button";
+import { ColumnsProps, TableProps } from "@components/Table";
+import { RepaymentScheduleDataPoint } from "@credix_types/repaymentschedule.types";
 
 interface AmortizationRepaymentScheduleProps {
 	children?: ReactNode;
@@ -17,60 +14,8 @@ export const AmortizationRepaymentSchedule: FunctionComponent<
 	AmortizationRepaymentScheduleProps
 > = () => {
 	const [showTable, setShowTable] = useState(false);
-
-	const data: RepaymentScheduleDataPoint[] = [];
-	for (let i = 1; i <= 12; i++) {
-		data.push({
-			month: i.toString(),
-			type: RepaymentScheduleAmountType.INTEREST,
-			amount: 120 - i * 10,
-		});
-
-		data.push({
-			month: i.toString(),
-			type: RepaymentScheduleAmountType.PRINCIPAL,
-			amount: i * 10,
-		});
-	}
-
-	const dataSource = [
-		{
-			date: "23 January",
-			principal: "200 000",
-			interest: "20 000",
-			balance: "1 780 000",
-		},
-		{
-			date: "23 Febrary",
-			principal: "443 000",
-			interest: "19 250",
-			balance: "1 570 000",
-		},
-		{
-			date: "23 March",
-			principal: "669 250",
-			interest: "18 150",
-			balance: "1 385 300",
-		},
-		{
-			date: "23 April",
-			principal: "xxxxx",
-			interest: "xxxxx",
-			balance: "xxxxx",
-		},
-		{
-			date: "23 May",
-			principal: "xxxxx",
-			interest: "xxxxx",
-			balance: "xxxxx",
-		},
-		{
-			date: "23 June",
-			principal: "xxxxx",
-			interest: "xxxxx",
-			balance: "xxxxx",
-		},
-	];
+	const [graphData] = useState<RepaymentScheduleDataPoint[]>([]);
+	const [dataSource] = useState<TableProps["dataSource"]>();
 
 	const columns: ColumnsProps[] = [
 		{
@@ -116,7 +61,7 @@ export const AmortizationRepaymentSchedule: FunctionComponent<
 					</div>
 				</div>
 				<div className="text-right">
-					<RepaymentScheduleGraph data={data} />
+					<RepaymentScheduleGraph data={graphData} />
 				</div>
 			</div>
 			{showTable && <RepaymentScheduleTable dataSource={dataSource} columns={columns} />}
