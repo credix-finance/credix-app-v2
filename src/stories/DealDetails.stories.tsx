444bf0eccfe98d0c231ffc48732a5aa8a65befb5
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-06-29 10:18:12 +0200

    fix: remove bigjs from stories as they dont work together
    https://github.com/storybookjs/storybook/issues/13163

--- src/stories/DealDetails.stories.tsx
+++ src/stories/DealDetails.stories.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { DealDetails } from "@components/DealDetails";
 import { PublicKey } from "@solana/web3.js";
-import Big from "big.js";
 import { Fraction } from "@credix/credix-client";
 
 export default {
@@ -16,12 +15,7 @@ export const Default = Template.bind({});
 Default.args = {
 	deal: {
 		borrower: new PublicKey("CRDx2YkdtYtGZXGHZ59wNv1EwKHQndnRc1gT4p8i2vPX"),
-		principal: new Big(1_000_000_000_000),
-		principalAmountRepaid: new Big(500_000_000_000),
 		financingFeePercentage: new Fraction(1, 8),
-		totalInterest: new Big(300_000_000_000),
-		interestToRepay: new Big(200_000_000_000),
-		interestRepaid: new Big(100_000_000_000),
 		isInProgress: () => true,
 		timeToMaturity: 360,
 		daysRemaining: 300,
