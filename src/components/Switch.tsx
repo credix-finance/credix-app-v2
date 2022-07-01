941428fe0a0f09b403fecd911cd071176252b5a1
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-07-01 14:02:26 +0200

    feat: add switch component

---
+++ src/components/Switch.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+import { Switch as AntSwitch } from "antd";
+import { FormItem, FormItemProps } from "./FormItem";
+
+interface SwitchProps {
+	name: FormItemProps["name"];
+	label: FormItemProps["label"];
+}
+export const Switch: FunctionComponent<SwitchProps> = ({ name, label }) => {
+	return (
+		<FormItem name={name} label={label} valuePropName="checked" itemClassName="switch">
+			<AntSwitch></AntSwitch>
+		</FormItem>
+	);
+};
