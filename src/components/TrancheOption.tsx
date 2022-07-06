06203b096b8b3da14f03ea84c98147566e889f96
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-07-06 12:13:13 +0200

    feat: update default tranche values

--- src/components/TrancheOption.tsx
+++ src/components/TrancheOption.tsx
@@ -38,18 +38,24 @@ export const TrancheOption: FunctionComponent<TrancheOptionProps> = ({ trancheDa
 	return (
 		<div className="flex space-x-12">
 			{donut}
-			<div className="grid grid-cols-3 w-1/2 gap-x-12 gap-y-2 text-sm">
+			<div className="grid grid-cols-4 w-1/2 gap-x-12 gap-y-2 text-sm">
 				<div className="col-span-1"></div>
 				<div className="col-span-1">
 					<div className="flex items-center space-x-1">
-						<Icon name="trend-up" size={IconDimension.SMALL} />
-						<span>Expected APY</span>
+						<Icon name="pie-chart" size={IconDimension.SMALL} />
+						<span>Principal</span>
 					</div>
 				</div>
 				<div className="col-span-1">
 					<div className="flex items-center space-x-1">
 						<Icon name="pie-chart" size={IconDimension.SMALL} />
-						<span>Size</span>
+						<span>Interest</span>
+					</div>
+				</div>
+				<div className="col-span-1">
+					<div className="flex items-center space-x-1">
+						<Icon name="trend-up" size={IconDimension.SMALL} />
+						<span>Expected APY</span>
 					</div>
 				</div>
 				{trancheData.map((tranche, index) => (
@@ -58,6 +64,8 @@ export const TrancheOption: FunctionComponent<TrancheOptionProps> = ({ trancheDa
 						name={tranche.name}
 						apr={tranche.apr}
 						value={tranche.value}
+						percentageOfPrincipal={tranche.percentageOfPrincipal}
+						percentageOfInterest={tranche.percentageOfInterest}
 						color={trancheColors[index]}
 						highlightedElement={highlightedElement}
 					/>
