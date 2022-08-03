e3d3e7ae2687f389fef0ae114bc5636efb1b43dd
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-08-03 15:51:17 +0200

    feat: disable video and screenshots when running cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  defaultCommandTimeout: 10000,
-  requestTimeout: 40000,
-  viewportHeight: 960,
-  viewportWidth: 1536,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-  },
-})
+	defaultCommandTimeout: 10000,
+	requestTimeout: 40000,
+	viewportHeight: 960,
+	viewportWidth: 1536,
+	video: false,
+	screenshotOnRunFailure: false,
+	e2e: {
+		// We've imported your old cypress plugins here.
+		// You may want to clean this up later by importing these.
+		setupNodeEvents(on, config) {
+			return require("./cypress/plugins/index.js")(on, config);
+		},
+	},
+});
